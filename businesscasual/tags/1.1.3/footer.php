52ab3582a74b5dc1ab3302909c8cb2f8cc8bc848
cpoteet <cpoteet@381ef2b2-7a2e-0410-83b3-e229eff1268e>
2008-08-12 04:51:07 +0000

    Tagged 1.1.3.

---
+++ businesscasual/tags/1.1.3/footer.php
@@ -0,0 +1,11 @@
+
+	</div>
+
+<div id="footer">
+<?php bloginfo('description'); ?>
+<br />Developed by <a href="http://www.siolon.com/">Siolon</a>
+<?php wp_footer(); ?>
+</div>
+</div>
+</body>
+</html>
